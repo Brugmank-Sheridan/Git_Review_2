4b5aa284862b519fce8889388ae12120e69c57a9
Brugmank-Sheridan <brugmank@sheridancollege.ca>
2020-01-23 14:43:23 -0500

    First Commit

---
+++ src/gitreview2/GitReview2.java
@@ -0,0 +1,28 @@
+/*
+        Name:  [your name here]
+        Assignment:  [assignment name]
+        Program: [your program name here]
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+
+package gitreview2;
+
+/**
+ *
+ * @author katarn
+ */
+public class GitReview2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Git Practice number 2");
+        // TODO code application logic here
+    }
+
+}
